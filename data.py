132acb47e738220c8d93317f4a2e3ca782f4fa66
BelindaElisabeth <mezbahpengintai12@gmail.com>
2025-02-17 10:10:44 +0300

    commit pertama

---
+++ data.py
@@ -0,0 +1,34 @@
+import streamlit as st 
+# import pandas as pd
+# import numpy as np
+st.json ( 
+{
+    'abc': '1',
+    'xyz': '2',
+    'pqr': '3'
+}
+)
+
+
+# df = pd.DataFrame(
+#     np.random.randn(10,5),
+#     columns=("col %d" % i for i in range(5)))
+# st.table(df)
+
+# st.write("This is a  DataFrame Demo")
+# df = pd.DataFrame(
+#     np.random.randn(50, 20),
+#     columns=('col %d' % i for i in range(20)))
+# st.dataframe(df)
+
+# st.title('I am title')
+# st.header("I am Header")
+# st.subheader("I am subheader")
+# st.markdown("I am markdown")
+# st.code("I am a coder")
+# st.text("I am a text")
+# st.write("I am write")
+# var1 ="Hello"
+# var2 = 5
+# st.write("Value of var2 =", var2)
+# st.write(var1)
